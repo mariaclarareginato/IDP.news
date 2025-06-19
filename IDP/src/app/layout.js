16ef8e6870971be155d93c4e674fc8317e59c712
Maria Clara Reginato <mariaclarareginato8@gmail.com>
2025-06-18 21:32:52 -0300

    resolvendo conflitos

---
+++ IDP/src/app/layout.js
@@ -0,0 +1,46 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import './globals.css';
+
+import { Roboto, Merriweather, Playfair_Display } from 'next/font/google';
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-roboto',
+});
+
+const merriweather = Merriweather({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-merriweather',
+});
+
+const playfairDisplay = Playfair_Display({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-playfair-display',
+});
+
+export default function RootLayout({ children }) {
+  const pathname = usePathname();
+
+  const showLayout = !(
+    pathname.startsWith('/Login') ||
+    pathname.startsWith('/Cadastro') ||
+    pathname.startsWith('/Esquecisenha')
+  );
+  
+  return (
+    <html lang="pt-br">
+      <body className={`${roboto.className} ${merriweather.className} ${playfairDisplay.className} bg-gray-100`}>
+        {showLayout && <Header />}
+        {children}
+        {showLayout && <Footer />}
+      </body>
+    </html>
+  );
+}
---
+++ IDP/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'ui-sans-serif', 'system-ui'],
+        merriweather: ['Merriweather', 'Georgia'],
+        display: ['Playfair Display', 'serif'],
+      },
+    },
+  },
+};
+
