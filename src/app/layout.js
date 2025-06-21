4674f685e269c97b678faffcc55288eeaff1a518
Maria Clara Reginato <mariaclarareginato8@gmail.com>
2025-06-21 19:51:55 -0300

    Tirando fontes

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,25 +5,6 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import './globals.css';
 
-import { Roboto, Merriweather, Playfair_Display } from 'next/font/google';
-
-const roboto = Roboto({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-roboto',
-});
-
-const merriweather = Merriweather({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-merriweather',
-});
-
-const playfairDisplay = Playfair_Display({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-playfair-display',
-});
 
 export default function RootLayout({ children }) {
   const pathname = usePathname();
